4dbfd17ea0945bf33287c0275c51ff3e9121254d
Katie Broida <kbroida@gmail.com>
2016-03-19 11:48:13 -0400

    separated app configuration, backend API and frontend routes into separate files. Started setup of handlebar templates

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express  = require('express');
-var app      = express();
+var app   = express();
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var exphbs  = require('express-handlebars');
 
 // configuration ===========================
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -13,15 +16,13 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-var routes = require('./routes/client.js');
+var client = require('./routes/client.js');
 var server = require('./routes/server.js');
+
+app.get('/', client.index);
+// app.get('/partials/:name', routes.partials);
 
 app.listen(8080);
 console.log("App listening on port 8080");
-
-app.use('/', server);
-
-// app.get('/', routes.index);
-// app.get('/partials/:name', routes.partials);
 
 module.exports = app;
